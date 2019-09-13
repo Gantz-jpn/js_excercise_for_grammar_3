4b20a47572e1595668ee69b38db39770315e2634
gantz <i.kiwa83@gmail.com>
2019-09-13 20:17:28 +0900

    add ; & shaping main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 //   - インクリメントを繰り返し変数numの値が100以上になったらWhileループを抜ける
 //     - デベロッパーツールのConsole上に0~99までの値が出力されたら正解
 let num = 0;
-while(num < 100) {
+while (num < 100) {
     console.log(num);
     num++;
 }
@@ -26,14 +26,14 @@ while(num < 100) {
 //       - answerとinputの値が同じだったら「正解です！」とalertで表示する
 //     - 正解が出るまでpromptで問題文を出し続ける
 //     - 正解が出たらWhileループを抜ける
-const question = 'パンはパンでも食べられないパンは？'
-const answer = 'フライパン'
+const question = 'パンはパンでも食べられないパンは？';
+const answer = 'フライパン';
 let input
-while(answer !== input) {
+while (answer !== input) {
     input = prompt(question);
-    if(answer === input) {
+    if (answer === input) {
         alert('正解です');
-    }else {
+    } else {
         alert('不正解です');
     }
 }
